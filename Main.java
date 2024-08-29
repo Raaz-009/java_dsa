b5175f571b20410eefd0e8e672ce6b3bbc9740a1
Raaz-009 <thameemraz829@gmail.com>
2024-08-29 23:52:35 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+    public static void main(String args[]){
+    
+    System.out.println("Addition:"+(3+3));
+    System.out.println("division:"+3/3);
+    System.out.println("Multiplication:"+3*3);
+    System.out.println("Subtraction"+(3-3));
+    }
+    }
