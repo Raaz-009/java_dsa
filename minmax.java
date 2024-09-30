d1cf50cfb883b32ea033c736c633ef7280bcdf83
Raaz-009 <thameemraz829@gmail.com>
2024-09-30 07:00:41 +0530

    min max or array

--- minmax.java
+++ minmax.java
@@ -1,16 +1,34 @@
 import java.util.Arrays;
+
+
 public class minmax {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int i;
-        int minsum = 0;
-        int maxsum = 0;
-        for(i=0; i<arr.length-1; i++){
-            minsum += arr[i];
+    public static void main(String[] args){
+    int[] arr={4,6,1,8,9,12,-4};
+
+    int min=arr[0];
+    int max=arr[0];
+
+    //using sort method
+    // Arrays.sort(arr);
+    // min =arr[0];
+    // max=arr[arr.length-1];
+
+    // System.out.println(min);
+    // System.out.println(max);
+
+
+
+    //using loops
+    
+    for(int i=1;i<arr.length;i++){
+        if(arr[i]<min){
+            min=arr[i];
         }
-        for(i=1; i<arr.length; i++){
-            maxsum += arr[i];
+        if(arr[i]>max){
+            max=arr[i];
         }
-        System.out.print(minsum+" "+maxsum);
     }
-}
+    System.out.println("min is :"+min);
+    System.out.println("max is :"+max);
+
+}}
---
+++ minmaxsum.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class minmaxsum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int i;
+        int minsum = 0;
+        int maxsum = 0;
+        for(i=0; i<arr.length-1; i++){
+            minsum += arr[i];
+        }
+        for(i=1; i<arr.length; i++){
+            maxsum += arr[i];
+        }
+        System.out.print(minsum+" "+maxsum);
+    }
+}
